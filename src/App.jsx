5c84ade1c0a52eed8d35e4443e5188edda0eee07
loscoy <1932536551@qq.com>
2024-01-15 15:26:14 +0800

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "./styles/index.scss";
 import "./styles/tailwind.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import AppRoutes from "./routes/appRoutes";
 import TestPage from "./pages/TestPage";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
+      <HashRouter basename="/">
         <AppRoutes />
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
